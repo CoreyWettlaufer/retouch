b718dd356b046c39caa6ae98abacc8a92589d4de
Corey Wettlaufer <corey.wettlaufer@gmail.com>
2018-04-08 13:58:36 -0400

    Update header.php to have nav directly underneath social icons

--- header.php
+++ header.php
@@ -15,21 +15,20 @@
                   <h1>Logo Will Go Here</h1>
                </div>
 
-               <div id="header-social" class="col col-lg-3 col-md-3 col-sm-4 align-self-center d-flex justify-content-end">
+               <div id="header-social" class="col col-lg-3 col-md-3 col-sm-4 align-self-center d-flex justify-content-end flex-column">
+                  <div class="social-section">
+                     <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/facebook-icon.png" alt="facebook icon"></a>
+                     <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/twitter-icon.png" alt="twitter icon"></a>
+                     <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/linked-in-icon.png" alt="linked in icon"></a>
+                     <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/google-plus-icon.png" alt="google plus icon"></a>
+                     <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/instagram-icon.png" alt="instagram icon"></a>
+                  </div>
 
-                  <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/facebook-icon.png" alt="facebook icon"></a>
-                  <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/twitter-icon.png" alt="twitter icon"></a>
-                  <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/linked-in-icon.png" alt="linked in icon"></a>
-                  <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/google-plus-icon.png" alt="google plus icon"></a>
-                  <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/instagram-icon.png" alt="instagram icon"></a>
+                  <nav id="mainNav" class="main-nav padding40-0" role="navigation">
+     	               <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => '' ) ); ?>
+     	            </nav>
 
                </div>
-             </div>
-
-             <div class="row justify-content-end padding0;">
-                <nav id="mainNav" class="main-nav" role="navigation">
-	               <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => '' ) ); ?>
-	            </nav>
              </div>
            </div>
         </header>
